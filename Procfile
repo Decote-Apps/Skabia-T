445f05538d689205a1781b7fb6896955e5512ff4
Rodrigo <morgan2990@gmail.com>
2015-05-31 17:33:14 -0300

    se crea de nuevo el proyecto

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -cp target/classes:target/dependency/* com.decote.skabiat.heroku.Main
---
+++ src/main/java/com/decote/skabiat/resources/BarResource.java
@@ -0,0 +1,25 @@
+package com.decote.skabiat.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * bar resource (exposed at "bar" path)
+ */
+@Path("bar")
+public class BarResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Hello, Heroku!";
+    }
+}
